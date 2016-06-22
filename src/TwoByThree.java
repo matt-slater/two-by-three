17fdb43938244c9c8589d4611d56dd557f860ec2
ekimlin <emmakimlin@gmail.com>
2016-06-21 23:38:06 -0400

    1st_updates
    No more duplicates!
    
    User has to input second CL arg to indicate where space goes
    
    Outputs with space between words

--- src/TwoByThree.java
+++ src/TwoByThree.java
@@ -17,7 +17,10 @@ public class TwoByThree {
 
         //get input from command line
         String puzzle = args[0];
-        globalInputString = puzzle;
+        checkUserPuzzle(puzzle);
+        String spacingIndicator = args[1]; 
+        String formattedPuzzle = formatPuzzleWithSpace(puzzle, spacingIndicator); 
+        globalInputString = formattedPuzzle;
 
         //erase letters from the alphabet array
         prepareAlphabet(puzzle);
@@ -28,12 +31,9 @@ public class TwoByThree {
     }
 
     static void iterateLetterCombos() {
-
-        for (int i = 0; i < abcs.size(); i++) {
-            for (int j = 0; j < abcs.size(); j++) {
-                if (i != j) {
-                    tryCombinations(abcs.get(i), abcs.get(j));
-                }
+        for (int i = 0; i < abcs.size() -1 ; i++) {
+            for (int j = i+1; j < abcs.size(); j++) {
+                tryCombinations(abcs.get(i), abcs.get(j));
             }
         }
     }
@@ -58,7 +58,6 @@ public class TwoByThree {
         for (int i = 0; i < combos.length; i++) {
             char[] currentCombo = globalInputString.toCharArray();
             replaceUnderscores(a, b, currentCombo, combos[i]);
-
         }
     }
 
@@ -73,10 +72,41 @@ public class TwoByThree {
                 }
                 counter++;
             }
-
-
         }
 
         System.out.println(String.valueOf(array));
     }
+
+    /*
+     * Checks "puzzle" provided by user to ensure it has 6 underscores. 
+     * Outputs error message and exits program if bad input.
+    */
+    static void checkUserPuzzle(String userPuzzle) {
+        int underscoreCount = 0; 
+        for (char c : userPuzzle.toCharArray() ) { // count number of underscores in userPuzzle
+            if (c == '_') ++underscoreCount; 
+        }
+        if (underscoreCount != 6) {
+            System.out.println("Invalid input. Puzzle requires six missing letters.\n");
+            System.exit(0);
+        }
+    }
+
+    /*
+     * Pre-condition: user_Puzzle is one word. spacingIndicator is in the format: 
+     *                 "<size of first word>,<size of second word>".
+     * Post-condition: returns a string equivalent to userPuzzle argument except 
+     *                 with a space inserted at index specified by spacingIndicator. 
+    */
+    static String formatPuzzleWithSpace(String userPuzzle, String spacingIndicator) {
+        //get length of first word
+        int endIndex = spacingIndicator.indexOf(','); 
+        String lenFirstWordStr = spacingIndicator.substring(0, endIndex);
+        int lenFirstWordInt = Integer.parseInt(lenFirstWordStr); 
+        //insert space
+        StringBuilder formattedUserPuzzle = new StringBuilder(userPuzzle); 
+        formattedUserPuzzle.insert(lenFirstWordInt, " ");  
+        return formattedUserPuzzle.toString(); 
+    }
+
 }
